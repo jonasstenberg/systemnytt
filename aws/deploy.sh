d977f0f33a585f52cc4d7410afb01b357e3ff1a9
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2018-11-27 21:42:49 +0100

    Add invalidation of CloudFront to deploy script

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Generate static files
 npm run build
 
+# Upload dist folder to s3
 cd dist && aws s3 cp . s3://systemnytt.se/dist --recursive --acl public-read --profile systemnytt --region eu-west-1
+
+# Invalidate cache in CloudFront
+aws cloudfront create-invalidation --distribution-id E1BBQ4CAGEREIL --paths "/*" --profile systemnytt
