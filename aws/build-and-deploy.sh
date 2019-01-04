64a0c63a082a89747de9e881715657187203adfc
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2019-01-04 11:55:45 +0100

    Update region

--- aws/build-and-deploy.sh
+++ aws/build-and-deploy.sh
@@ -4,7 +4,7 @@
 npm run build
 
 # Upload dist folder to s3
-cd dist && aws s3 cp . s3://systemnytt.se/ --recursive --acl public-read --profile systemnytt --region eu-west-1
+cd dist && aws s3 cp . s3://systemnytt.se/ --recursive --acl public-read --region eu-north-1 --profile systemnytt
 
 # Invalidate cache in CloudFront
 aws cloudfront create-invalidation --distribution-id E1BBQ4CAGEREIL --paths "/*" --profile systemnytt
