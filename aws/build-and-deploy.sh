0bf8d966c2c80ace0b6c93cfff901ae2221e0967
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2019-02-24 10:18:43 +0100

    Use s3 sync instead of cp

--- aws/build-and-deploy.sh
+++ aws/build-and-deploy.sh
@@ -4,7 +4,7 @@
 npm run build
 
 # Upload dist folder to s3
-cd dist && aws s3 cp . s3://systemnytt.se/ --recursive --acl public-read --region eu-north-1 --profile systemnytt
+cd dist && aws s3 sync . s3://systemnytt.se/ --acl public-read --region eu-north-1 --profile systemnytt --delete
 
 # Invalidate cache in CloudFront
 aws cloudfront create-invalidation --distribution-id E1BBQ4CAGEREIL --paths "/*" --profile systemnytt
